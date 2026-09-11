025710da9322e7883ddda72d5accac30659345c8
Facundo Alejandro Saenz Figueroa <6288469@cegepmontpetit.ca>
2026-09-11 15:05:10 -0400

    PROGRÈS Debut de l'exercice
    Il faut le finir !!

--- polling-et-signalr/app/polling/page.tsx
+++ polling-et-signalr/app/polling/page.tsx
@@ -13,25 +13,34 @@ export default function Home() {
     updateTasks();
   }, []);
 
-  function handleTaskAdd(taskName: string) {
+  async function handleTaskAdd(taskName: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    // const appelServeur = await axios.post('http://localhost:5042/api/UselessTasks/Add', {
+    //   taskName
+    // });
+    const appelServeur = await axios.post(`http://localhost:5042/api/UselessTasks/Add?taskText=${taskName}`);
+    console.log(appelServeur.data);
   }
 
-  function onTaskToggle(id: number) {
+  async function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
-
+    const appelServeur = await axios.get('http://localhost:5042/api/UselessTasks/Complete/' + id);
+    console.log(appelServeur.data);
     let tasksCopy : UselessTask[] = [...tasks];    
     tasksCopy.find(task => task.id === id)!.completed = true;
     setTasks(tasksCopy);
   }
 
   async function updateTasks() {
-    let testTasks = new Array<UselessTask>(
-      { id: 1, text: "Test Task 1", completed: false },
-      { id: 2, text: "Test Task 2", completed: true });
-    setTasks(testTasks);
+    // let testTasks = new Array<UselessTask>(
+    //   { id: 1, text: "Test Task 1", completed: false },
+    //   { id: 2, text: "Test Task 2", completed: true });
+    
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    const appelServeur = await axios.get('http://localhost:5042/api/UselessTasks/GetAll');
+    console.log(appelServeur.data);
+    setTasks(appelServeur.data);
   }
 
   return (
